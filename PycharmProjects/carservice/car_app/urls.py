e69e4081732d9f2cd07be82c88d0fdf72613be09
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-05-03 18:09:09 +0300

    Adding car, car_models, order and orders templates

--- PycharmProjects/carservice/car_app/urls.py
+++ PycharmProjects/carservice/car_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cars', views.cars, name='cars'),
+    path('car/<int:car_id>', views.car, name='car'),
+    path('orders/', views.orders, name='orders'),
+    path('order/<int:order_list_id>', views.order, name='order'),
 ]
